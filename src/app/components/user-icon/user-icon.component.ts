5beea2aea55cb6613c4738ba3a7ef07105b378f4
Walter Taño <tano.walter@hotmail.com>
2022-09-19 19:30:01 -0300

    Refactoricé para agregar user-icon

---
+++ src/app/components/user-icon/user-icon.component.ts
@@ -0,0 +1,17 @@
+import { User } from './../../interfaces/User';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-icon',
+  templateUrl: './user-icon.component.html',
+  styleUrls: ['./user-icon.component.scss']
+})
+export class UserIconComponent implements OnInit {
+  @Input() user!: User;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
